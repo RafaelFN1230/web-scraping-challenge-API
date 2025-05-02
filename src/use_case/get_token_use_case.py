dcb245f1e5ca51f03aa5894b0684b00581822450
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2025-05-01 22:24:40 -0300

    Refactor: Changing name of the generate token use case to a more apropriate get token use case

--- src/use_case/get_token_use_case.py
+++ src/use_case/get_token_use_case.py
@@ -1,5 +1,5 @@
 from infrastructure.API.get_token import get_token
 from domain.entities.User import User
 
-def generate_token_use_case(base_url: str, user: User) -> str:
+def get_token_use_case(base_url: str, user: User) -> str:
     return get_token(base_url, user)
