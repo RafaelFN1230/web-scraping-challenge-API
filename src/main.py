f4a274c36b3983d89c6322159734d3d0cb1b94b2
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2025-05-02 12:17:59 -0300

    Refactor: Refactoring the main file, in a way that make the code more readable

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from interface.controller.main_controller import MainController
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
-        
-if __name__ == "__main__":
+
+def main():
     logger.info("Iniciando aplicação.")
     base_url = "https://demoqa.com"
     controller = MainController(base_url=base_url)
@@ -11,5 +11,7 @@ if __name__ == "__main__":
         controller.execute(csv_path="output/books_api.csv")
     except Exception as e:
         logger.error(f"Erro: {e}", exc_info=True)
-
     logger.info("Processo finalizado com sucesso.")
+
+if __name__ == "__main__":
+    main()
