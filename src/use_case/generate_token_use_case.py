d42de01aa84978be28a950411f79d7d61bbb87d0
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2025-05-01 22:23:02 -0300

    Feat: Adds a use case that get the token information

---
+++ src/use_case/generate_token_use_case.py
@@ -0,0 +1,5 @@
+from infrastructure.API.get_token import get_token
+from domain.entities.User import User
+
+def generate_token_use_case(base_url: str, user: User) -> str:
+    return get_token(base_url, user)
